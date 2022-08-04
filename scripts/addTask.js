cb10799d199af520a7495d8e3e1d36db9866a5a6
Maximilian Bruhn <maximilianbruhn@gmx.de>
2022-08-04 11:38:07 +0200

    create add Task Page

---
+++ scripts/addTask.js
@@ -0,0 +1,18 @@
+let users = ['avatar1.png', 'avatar2.png', 'avatar3.png']
+
+
+function renderAvatarPicker() {
+    document.getElementById('avatarPicker').innerHTML = '';
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        document.getElementById('avatarPicker').innerHTML += `<img id="user-${i}" src="img/avatars/${user}" class="avatar" onclick="selectUser(${i})">`
+
+    }
+    document.getElementById('avatarPicker').innerHTML += ` <img class="avatar" src="img/icons8-plus.png" alt="">`;
+}
+
+function selectUser(i) {
+
+    let user = document.getElementById('user-' + i)
+    user.classList.toggle('avatar-selected');
+}
