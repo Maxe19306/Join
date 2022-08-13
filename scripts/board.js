2d7f27193eee7d8942eb979ce165d8c2c6a3a215
unknown <thomas_ketler@web.de>
2022-08-13 19:05:34 +0200

    fixed-the-trash-button

--- scripts/board.js
+++ scripts/board.js
@@ -62,8 +62,10 @@ function filterDone(currentDone) {
 }
 
 
-async function deleteTaskOnBoard(index) {
-    allTask.splice(index, 1); 
+async function deleteTaskOnBoard(i) {
+    let deleteTask = allTask.findIndex(obj => obj.createdAt == i);
+    console.log('findindex', deleteTask);
+    allTask.splice(deleteTask, 1);
     await backend.deleteItem('allTask');
     loadAllFilter();
     pushAllTask();
@@ -71,7 +73,6 @@ async function deleteTaskOnBoard(index) {
 
 
 function trashOpen(i, index) {
-    console.log('antwort', i, index);
     document.getElementById(`trashAktiv ${i} ${index['state']}`).classList.remove(`d-none`);
 }
 
@@ -113,7 +114,7 @@ function htmlTicket(i, index) {
                         </span>
                     </div>
                     <div>
-                        <svg id="trashAktiv ${i} ${index['state']}" onclick="deleteTaskOnBoard(${i})" class="trash-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <svg id="trashAktiv ${i} ${index['state']}" onclick="deleteTaskOnBoard(${currentDraggedElement})" class="trash-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                             <path 
                                 d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/>
